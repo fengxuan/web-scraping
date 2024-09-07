a24b6cbbf4061ca020ccd474b94c9563278e5034
FengXuan <xuanjianfeng@163.com>
2024-09-07 08:25:33 +0800

    test

--- testwchat.js
+++ testwchat.js
@@ -17,9 +17,9 @@ function fetchAndProcessUrl(url, filename) {
           .join("\n");
 
         // var body = $("rich_media_content");
-        console.log(richMediaContent);
+        // console.log(richMediaContent);
 
-        var text = "test"; //stripHtml(body).result;
+        var text = richMediaContent; //stripHtml(body).result;
         console.log(text);
 
         if (!filename) {
@@ -38,5 +38,10 @@ function fetchAndProcessUrl(url, filename) {
   );
 }
 
+var url =
+  "https://mp.weixin.qq.com/s?src=11&timestamp=1725668101&ver=5491&signature=xvxBCZ6TR6sElDfOBFMUmRnI4VpKcao-dzCqreofaqB69B-WnnaJZuimG8ES2Lx-mStejNZzZoV0Sl5kcuQYIafsXc2WIWR-RYWkE8fde-lJR5ITvCNsvke1LyaP-0u9&new=1";
 // Usage example:
-fetchAndProcessUrl("https://mp.weixin.qq.com/s/ASbjIF2aCmKK5rRFdnzIBQ");
+// url = "https://mp.weixin.qq.com/s/ASbjIF2aCmKK5rRFdnzIBQ"
+url =
+  "https://mp.weixin.qq.com/s?src=11&timestamp=1725668323&ver=5491&signature=tl6F-QBWaMy4E409pnpDmJyAVoHpRzQmlVMcFK7CP-OE3513UrtU6oLZdCQMquirgnOwfFtpsuh8trJQDVkrLt7Vp6Y*nO7yShzbxuSBJjK3B1hRUdjyAB7nc9w6u2tP&new=1";
+fetchAndProcessUrl(url);
